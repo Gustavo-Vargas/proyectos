d5ffa4caadbaa05fd1b3ff4a1cc4a853ff65d67a
Gustavo R. Vargas <ing.gustavo.rv@gmail.com>
2025-10-30 12:53:49 -0600

    Agrear validacion de si existe foto y datos en datospersonales

--- videojuegos/usuarios/views.py
+++ videojuegos/usuarios/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.views import LoginView
 from django.contrib.auth.forms import AuthenticationForm
 # from .froms import LoginForm
 from django.views.generic import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from .forms import FromDatosPersonales, UserForm
 from django.urls import reverse_lazy
@@ -21,15 +22,28 @@ class RegistrarView(SuccessMessageMixin, CreateView):
     success_url = reverse_lazy('login')
     success_message = "%(username)s se ha registrado de manera exitosa"
 
-class CrearPerfilView(SuccessMessageMixin, CreateView):
+class CrearPerfilView(SuccessMessageMixin, LoginRequiredMixin, CreateView):
     model = DatosPersonales
     form_class = FromDatosPersonales
     success_url = reverse_lazy('bienvenida')
     success_message = "Se guardaron tus datos personales"
     
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        try:
+            kwargs['instance'] = DatosPersonales.objects.get(user=self.request.user)
+        except DatosPersonales.DoesNotExist:
+            pass
+        return kwargs
+
     def form_valid(self, form):
         datos_personales = form.save(commit=False)
-        datos_personales.user = self.request.user
+        if not datos_personales.pk:
+            datos_personales.user = self.request.user
+        if datos_personales.pk:
+            anterior = DatosPersonales.objects.get(pk=datos_personales.pk)
+            if anterior.foto and form.cleaned_data.get('foto') and anterior.foto.name != form.cleaned_data['foto'].name:
+                anterior.foto.delete(save=False)
         datos_personales.save()
         
         return super().form_valid(form)
--- videojuegos/videojuegos/settings.py
+++ videojuegos/videojuegos/settings.py
@@ -115,6 +115,9 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static/'),
 )
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 LOGIN_URL = reverse_lazy('login')
 LOGIN_REDIRECT_URL = reverse_lazy('bienvenida')
 LOGOUT_REDIRECT_URL = reverse_lazy('login')
