b45e7216c881b988add0eaa73b0e03d7c4db4714
Gustavo R. Vargas <ing.gustavo.rv@gmail.com>
2025-11-07 22:54:59 -0600

    Modifica la función asignaUsuario para aceptar grupos y actualiza el botón de asignación de grupos en la plantilla lista_usuarios.html para pasar los grupos seleccionados.

--- videojuegos/static/js/funciones.js
+++ videojuegos/static/js/funciones.js
@@ -1,3 +1,11 @@
-function asignaUsuario(idUsuario){
+function asignaUsuario(idUsuario, grupos) {
     document.getElementById('idUsuario').value = idUsuario;
+    const seleccionados = grupos
+        .split('-')
+        .map(valor => valor.trim())
+        .filter(Boolean);
+    $('#formGrupos input[type=checkbox]').each(function () {
+        const estaSeleccionado = seleccionados.includes($(this).prop('name'));
+        $(this).prop('checked', estaSeleccionado);
+    });
 }
